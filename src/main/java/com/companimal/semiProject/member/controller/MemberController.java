ea0501f9df2905ffc90ed5ae97ee20a8b4c76f96
antmrhdqn <antmrhdqn@gmail.com>
2024-03-07 12:24:52 +0900

    1

---
+++ src/main/java/com/companimal/semiProject/member/controller/MemberController.java
@@ -0,0 +1,71 @@
+package com.companimal.semiProject.member.controller;
+
+import com.companimal.semiProject.member.model.dto.MemberDTO;
+import com.companimal.semiProject.member.model.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/member")
+public class MemberController {
+
+    @Autowired
+    private MemberService memberService;
+
+    @GetMapping("/regist")
+    public void regist() {
+    }
+
+    @PostMapping("/regist")
+    public ModelAndView regist(ModelAndView mv, @ModelAttribute MemberDTO memberDTO) {
+
+        int result = memberService.regist(memberDTO);
+
+        String message ="";
+
+        if (result > 0) {
+            message = "회원가입이 정상적으로 완료되었습니다.";
+            mv.setViewName("auth/login");
+        } else {
+            message = "회원가입에 실패하였습니다.";
+            mv.setViewName("user/regist");
+        }
+
+        mv.addObject("message", message);
+
+        return mv;
+    }
+
+    @RequestMapping("/mypage")
+    public String showMypage() {
+        System.out.println("마이페이지 확인");
+
+        return "/contents/member/supportermypage";
+    }
+
+    @RequestMapping("/coupon")
+    public String selectCouponList() {
+        System.out.println("쿠폰 목록");
+
+        return "contents/member/supportercoupon";
+    }
+
+    @RequestMapping("/profile")
+    public String showProfile() {
+        System.out.println("프로필 확인");
+
+        return "contents/member/supporterProfile";
+    }
+
+    @RequestMapping("/noticecenter")
+    public String showNoticecenter() {
+        System.out.println("알림 센터");
+
+        return "contents/member/noticecenter";
+    }
+}
