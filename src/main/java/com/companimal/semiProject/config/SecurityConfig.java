1e77c512510c1754420f862f4eb9d4252734075c
Jee Hwan <jeehwan98@naver.com>
2024-03-07 11:43:52 +0900

    24-03-07 jee 02

--- src/main/java/com/companimal/semiProject/config/SecurityConfig.java
+++ src/main/java/com/companimal/semiProject/config/SecurityConfig.java
@@ -1,4 +1,9 @@
 package com.companimal.semiProject.config;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 }
--- src/main/java/com/companimal/semiProject/config/WebConfig.java
+++ src/main/java/com/companimal/semiProject/config/WebConfig.java
@@ -1,4 +1,8 @@
 package com.companimal.semiProject.config;
 
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
 public class WebConfig {
 }
