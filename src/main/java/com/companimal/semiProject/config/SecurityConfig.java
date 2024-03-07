10ca05314a8d85b75a847b4fcfd08705fd183b84
antmrhdqn <antmrhdqn@gmail.com>
2024-03-07 11:29:03 +0900

    0307-dh-000

--- src/main/java/com/companimal/semiProject/config/SecurityConfig.java
+++ src/main/java/com/companimal/semiProject/config/SecurityConfig.java
@@ -1,4 +1,58 @@
 package com.companimal.semiProject.config;
 
+import com.companimal.semiProject.common.MemberRole;
+import com.companimal.semiProject.config.handler.AuthFailHandler;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
+
+    @Autowired
+    private AuthFailHandler authFailHandler;
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+
+        http.authorizeHttpRequests(auth -> {
+            auth.requestMatchers("/auth/login", "/member/regist", "/auth/fail", "/main").permitAll();
+            auth.requestMatchers("manager/*").hasAnyAuthority(MemberRole.MANAGER.getRole());
+            auth.requestMatchers("member/*").hasAnyAuthority(MemberRole.CREATOR.getRole(), MemberRole.SUPPORTER.getRole());
+            auth.requestMatchers("creator/*").hasAnyAuthority(MemberRole.CREATOR.getRole());
+            auth.anyRequest().authenticated();
+        }).formLogin(login -> {
+            login.loginPage("/auth/login");
+            login.usernameParameter("user");
+            login.passwordParameter("pass");
+            login.defaultSuccessUrl("/", true);
+            login.failureHandler(authFailHandler);
+
+            /* 메인페이지 로그아웃 버튼에  <form th:action="@{/auth/logout}" th:method="post"> 써야함*/
+        }).logout(logout -> {
+            logout.logoutRequestMatcher(new AntPathRequestMatcher("/auth/logout"));
+            logout.deleteCookies("JSESSIONID");
+            logout.invalidateHttpSession(true);
+            logout.logoutSuccessUrl("/");
+
+        }).sessionManagement(session -> {
+            session.maximumSessions(1);
+            session.invalidSessionUrl("/");
+
+        }).csrf(csrf -> csrf.disable());
+
+        return http.build();
+    }
 }
--- src/main/java/com/companimal/semiProject/order/controller/OrderController.java
+++
@@ -1,4 +0,0 @@
-package com.companimal.semiProject.order.controller;
-
-public class ProjectController {
-}
