ec6c37d6d88ce7cd9d9dea01c2baa5d4d6244179
jeonghee <hongi_@naver.com>
2024-03-07 11:07:25 +0900

    24-03-07 jeong 001

---
+++ src/main/java/com/companimal/semiProject/config/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.companimal.semiProject.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan(basePackages = "com.companimal.semiProject")
+public class SpringConfig {
+}
--- src/main/java/com/companimal/semiProject/project/model/service/ProjectService.java
+++ src/main/java/com/companimal/semiProject/project/model/service/ProjectService.java
@@ -1,4 +1,9 @@
 package com.companimal.semiProject.project.model.service;
 
+import com.companimal.semiProject.project.model.dto.ProjectDTO;
+
+import java.util.List;
+
 public interface ProjectService {
+    List<ProjectDTO> selectProject();
 }
