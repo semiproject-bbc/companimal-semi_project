9023909970bf7677c803fa02a7451e10268b42dd
antmrhdqn <antmrhdqn@gmail.com>
2024-03-08 14:25:47 +0900

    security check

--- src/main/java/com/companimal/semiProject/main/controller/MainController.java
+++ src/main/java/com/companimal/semiProject/main/controller/MainController.java
@@ -1,14 +1,16 @@
 package com.companimal.semiProject.main.controller;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
 
     @GetMapping(value = {"/", "/main"})
-    public String main() {
+    public String home(Model model) {
         return "main";
     }
-
 }
